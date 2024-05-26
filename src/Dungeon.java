81c2469e3fc97ddec0434b80e0c618dd4073b244
mistletoe0 <145917617+mistletoe0@users.noreply.github.com>
2024-05-26 16:09:52 +0800

    Update Dungeon.java

--- src/Dungeon.java
+++ src/Dungeon.java
@@ -1,10 +1,6 @@
 import java.util.ArrayList;
-<<<<<<< HEAD
-import java.util.Arrays;
-=======
 import java.lang.Character;
 
->>>>>>> bf73991 (Initial commit)
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,19 +15,6 @@ public class Dungeon {
     // initialize a new dungeon based on the given board
     public Dungeon(char[][] board) {
         N = board.length;
-<<<<<<< HEAD
-        isRoom     = new boolean[N][N];
-        isCorridor = new boolean[N][N];
-        adjMap     = new HashMap<>();
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if      (board[i][j] == '.') isRoom[i][j] = true;
-                else if (board[i][j] == '+') isCorridor[i][j] = true;
-            }
-        }
-
-=======
         isRoom = new boolean[N][N];
         isCorridor = new boolean[N][N];
         adjMap = new HashMap<>();
@@ -51,7 +34,6 @@ public class Dungeon {
         }
 
 
->>>>>>> bf73991 (Initial commit)
         // traverse each node of the dungeon
         // Check the point to the surrounding grid is valid or not
         for(int i = 0; i < N; i++) {
@@ -137,25 +119,6 @@ public class Dungeon {
     // return dimension of dungeon
     public int size() { return N; }
 
-<<<<<<< HEAD
-    // does v correspond to a corridor site? 
-    public boolean isCorridor(Site v) {
-        int i = v.i();
-        int j = v.j();
-        return i >= 0 && j >= 0 && i < N && j < N && isCorridor[i][j];
-    }
-
-    // does v correspond to a room site?
-    public boolean isRoom(Site v) {
-        int i = v.i();
-        int j = v.j();
-        return i >= 0 && j >= 0 && i < N && j < N && isRoom[i][j];
-    }
-
-    // does v correspond to a wall site?
-    public boolean isWall(Site v) {
-        return (!isRoom(v) && !isCorridor(v));
-=======
     // does v correspond to a corridor site?
     public boolean isRoom(Site v) {
         int i = v.i();
@@ -179,7 +142,6 @@ public class Dungeon {
         boolean result = (!isRoom(v) && !isCorridor(v));
         //System.out.println("isWall check for " + v + ": " + result);
         return result;
->>>>>>> bf73991 (Initial commit)
     }
 
     // does v-w correspond to a legal move?
@@ -188,20 +150,6 @@ public class Dungeon {
         int j1 = v.j();
         int i2 = w.i();
         int j2 = w.j();
-<<<<<<< HEAD
-        if (i1 < 0 || j1 < 0 || i1 >= N || j1 >= N) return false;
-        if (i2 < 0 || j2 < 0 || i2 >= N || j2 >= N) return false;
-        if (isWall(v) || isWall(w)) return false;
-        if (Math.abs(i1 - i2) > 1)  return false;
-        if (Math.abs(j1 - j2) > 1)  return false;
-        if (isRoom(v) && isRoom(w)) return true;
-        if (i1 == i2)               return true;
-        if (j1 == j2)               return true;
-
-        return false;
-    }
-
-=======
 
        /* System.out.println("Checking move from " + v + " to " + w);
         System.out.println("i1: " + i1 + ", j1: " + j1);
@@ -247,5 +195,4 @@ public class Dungeon {
 
 
 
->>>>>>> bf73991 (Initial commit)
 }
