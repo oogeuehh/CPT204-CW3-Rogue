5dcacfa769145edd3ff3b9352a653e8e39af00dd
Doophie <477599086@qq.com>
2024-06-01 22:25:04 +0800

    final version

--- src/Game.java
+++ src/Game.java
@@ -4,7 +4,7 @@ public class Game {
 
     // portable newline
     private final static String NEWLINE = System.getProperty("line.separator");
-  
+
     private Dungeon dungeon;     // the dungeon
     private char MONSTER;        // name of the monster (A - Z)
     private char ROGUE = '@';    // name of the rogue
@@ -98,12 +98,12 @@ public class Game {
             else throw new RuntimeException("Rogue caught cheating");
             System.out.println(this);
 
-//            // make process visible
-//            try {
-//                Thread.sleep(500);
-//            } catch (InterruptedException e) {
-//                throw new RuntimeException(e);
-//            }
+            // make process visible
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
 
         System.out.println("Caught by monster");
